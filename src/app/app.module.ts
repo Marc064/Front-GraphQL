71cd394add69714649d03e65cc16b79fcc6d31a8
Andres Amaya <andre.amaya03@uptc.edu.co>
2024-03-02 09:47:40 -0500

    Creacion del componente eliminar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarComponent } from './listar/listar.component';
+import { EliminarComponent } from './eliminar/eliminar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListarComponent
+    ListarComponent,
+    EliminarComponent
   ],
   imports: [
     BrowserModule,
