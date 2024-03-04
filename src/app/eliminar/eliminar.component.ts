7268e34cdcc74a0deee369f894e523d773d45d0b
Andres Amaya <andre.amaya03@uptc.edu.co>
2024-03-03 21:58:18 -0500

    Componente Eliminar implementado

--- src/app/eliminar/eliminar.component.ts
+++ src/app/eliminar/eliminar.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,Input} from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+const deleteComputeMutation = gql`
+  mutation DeleteCompute($deleteComputeId: ID!) {
+    deleteCompute(id: $deleteComputeId) {
+      id
+    }
+  }
+`;
 
 @Component({
   selector: 'app-eliminar',
@@ -6,5 +15,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./eliminar.component.css']
 })
 export class EliminarComponent {
+  @Input()id: string = "";
 
+  constructor(private apollo: Apollo) {}
+
+  deleteData() {
+    this.apollo.mutate({
+      mutation: deleteComputeMutation,
+      variables: {
+        deleteComputeId: this.id
+      }
+    }).subscribe({
+      next: (result) => {
+        console.log('Datos eliminados:', result.data);
+        alert('Computador eliminado');
+        this.id = ""; 
+        window.location.reload();
+      },
+      error: (error) => {
+        alert('Error al eliminar los datos.');
+      }
+    });
+  }
 }
